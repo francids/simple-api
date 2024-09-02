f4fa7f8f30903a520b1828072c46289ecc36aa71
francids <francisco.mc.ids@gmail.com>
2024-09-02 14:01:52 -0400

    feat: add endpoint to retrieve a specific blog post

--- routes/posts.py
+++ routes/posts.py
@@ -12,3 +12,14 @@ async def get_posts() -> object:
         posts_data = json.load(file)
 
     return JSONResponse(content=jsonable_encoder(posts_data))
+
+
+@router.get("/posts/{post_id}")
+async def get_post(post_id: int) -> object:
+    with open("data/posts.json", "r") as file:
+        posts_data = json.load(file)
+
+    post_data = [post for post in posts_data if post["id"] == post_id] or None
+    if not post_data:
+        return JSONResponse(content={"error": "Post not found"}, status_code=404)
+    return JSONResponse(content=jsonable_encoder(post_data))
