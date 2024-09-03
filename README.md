e479a985578ace1ef0d5431b614f7ac0e4136908
francids <francisco.mc.ids@gmail.com>
2024-09-03 12:30:13 -0400

    style: modularize routes and update file structure in app for scalability

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ pip install -r requirements.txt
 ## Usage
 
 ```bash
-uvicorn app:app --host 0.0.0.0 --port $PORT
+uvicorn app.main:app --host 0.0.0.0 --port $PORT
 ```
 
 ## Routes
--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from routes.users import router as users_router
-from routes.posts import router as posts_router
-
+from app.routes.users import router as users_router
+from app.routes.posts import router as posts_router
 
 app: FastAPI = FastAPI()
 
